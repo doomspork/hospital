94c12337cdb211d69882baf04feeaf53af6822e4
Sean Callan <seancallan@gmail.com>
2015-09-29 15:04:33 -0700

    HealthCheck API should support Users and Medics

--- web/controllers/api/health_check_controller.ex
+++ web/controllers/api/health_check_controller.ex
@@ -4,15 +4,32 @@ defmodule Hospital.Api.HealthCheckController do
   import Ecto.Query
 
   alias Hospital.HealthCheck
+  alias Hospital.Medic
   alias Hospital.SessionController
+  alias Hospital.User
 
   plug Guardian.Plug.EnsureAuthenticated, %{ on_failure: { SessionController, :unauthenticated_api } }
-  plug Guardian.Plug.EnsurePermissions, medic: [:read], on_failure: { SessionController, :forbidden_api }
+  plug Guardian.Plug.EnsurePermissions, default: [:read], on_failure: { SessionController, :forbidden_api }
 
   def index(conn, _params) do
+    health_checks = conn
+                    |> Guardian.Plug.current_resource
+                    |> health_checks_query
+    render(conn, "index.json", health_checks: health_checks)
+  end
+
+  defp health_checks_query(%User{id: id}) do
+    query = from h in HealthCheck,
+            select: h,
+            where: h.user_id == ^id
+
+    Repo.all(query)
+  end
+
+  defp health_checks_query(%Medic{}) do
     query = from h in HealthCheck,
             select: h
-    health_checks = Repo.all(query)
-    render(conn, "index.json", health_checks: health_checks)
+
+    Repo.all(query)
   end
 end
--- web/views/api/health_check_view.ex
+++ web/views/api/health_check_view.ex
@@ -2,11 +2,11 @@ defmodule Hospital.Api.HealthCheckView do
   use Hospital.Web, :view
 
   def render("index.json", %{health_checks: health_checks}) do
-    %{data: render_many(health_checks, Hospital.HealthCheckView, "health_check.json")}
+    %{data: render_many(health_checks, Hospital.Api.HealthCheckView, "health_check.json")}
   end
 
   def render("show.json", %{health_check: health_check}) do
-    %{data: render_one(health_check, Hospital.HealthCheckView, "health_check.json")}
+    %{data: render_one(health_check, Hospital.Api.HealthCheckView, "health_check.json")}
   end
 
   def render("health_check.json", %{health_check: health_check}) do
