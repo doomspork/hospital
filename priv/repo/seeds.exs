521b566858bac1eab0eb7902fc529e99537eeb2d
Sean Callan <seancallan@gmail.com>
2015-09-30 09:16:21 -0700

    Basic db seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Hospital.Repo
+alias Hospital.User
+alias Hospital.HealthCheck
+
+user = %User{}
+       |> User.create_changeset(%{email: "user@example.com", name: "User", password: "p@ssword", password_confirmation: "p@ssword"})
+       |> Repo.insert!
+
+%HealthCheck{name: "Example One", target: "127.0.0.1", type: "ping", user_id: user.id} |> Repo.insert!
+%HealthCheck{name: "Another Example", target: "http://example.com", type: "get", user_id: user.id} |> Repo.insert!
