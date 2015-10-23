8f6137dc9e3ecf8fe5b41706de908e4914b39811
Sean Callan <seancallan@gmail.com>
2015-10-23 12:53:31 -0700

    Update webpack to handle css

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './web/static/js/app.js',
+  entry: ['./web/static/js/app.js', './web/static/css/app.css'],
   output: {
-    path: './priv/static/js',
-    filename: 'bundle.js'
+    path: './priv/static',
+    filename: 'js/app.js'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('css/app.css')
   ],
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      }
-    ]
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader'],
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('css')
+    }]
   }
 };
